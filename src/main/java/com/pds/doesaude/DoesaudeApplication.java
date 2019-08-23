83d26d9d3dee9d874b8685fe70e85717ab4822f2
Luiz Eduardo <luizeduardob@live.com>
2019-08-22 21:31:47 -0300

    Projeto criado

---
+++ src/main/java/com/pds/doesaude/DoesaudeApplication.java
@@ -0,0 +1,13 @@
+package com.pds.doesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoesaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoesaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pds/doesaude/DoesaudeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pds.doesaude;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoesaudeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
