38cd5aa60fe1abc40d681bd189208c126a7adad5
Luiz Eduardo <luizeduardob@live.com>
2019-09-03 18:19:46 -0300

    Criando operação de instanciacao

--- src/main/java/com/pds/doesaude/DoesaudeApplication.java
+++ src/main/java/com/pds/doesaude/DoesaudeApplication.java
@@ -1,13 +1,29 @@
 package com.pds.doesaude;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DoesaudeApplication {
+import com.pds.doesaude.domain.Categoria;
+import com.pds.doesaude.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DoesaudeApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DoesaudeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
